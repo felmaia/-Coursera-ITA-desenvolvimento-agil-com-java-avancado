a91480c7a05e3bdeccdea056dd5eca77c7c6f223
maiaf <maiaf@DESKTOP-DLKP688>
2016-10-25 15:49:39 -0200

    commit projeto ContadoresEscopo

---
+++ src/pojo/Contador.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class Contador {
+
+	private int numeroAtual = 0;
+	private String nome;
+	
+	public Contador(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public void contar() {
+		numeroAtual++;
+	}
+
+	@Override
+	public String toString() {
+		return "Contador [numeroAtual=" + numeroAtual + ", nome=" + nome + "]";
+	}
+
+}
